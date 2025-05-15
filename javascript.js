1f22648693dd1af0c0e3f46229bb6738245b4cc4
MakenzieBuchenroth-Neumont <mbuchenroth@student.neumont.edu>
2025-05-15 14:25:09 -0600

    optimized code.

--- javascript.js
+++ javascript.js
@@ -1,56 +1,84 @@
-$(document).ready(function() {
-  $('i').hide();
-})
+'use strict';
 
-$(window).load(function() {
-  $('i').show();
+// Animation configuration
+const ANIMATION_CONFIG = {
+    duration: {
+        bounce: 500,
+        return: 250
+    },
+    delay: {
+        linkedin: 250,
+        github: 500,
+        itch: 750
+    },
+    offset: {
+        linkedin: { top: 10, left: -10 },
+        github: { top: 10, left: -6 },
+        itch: { top: 10, left: -3 }
+    }
+};
 
-  var githubPos = $('#github').position();
-  var linkedinPos = $('#linkedin').position();
-  var itchPos = $('#itch').position();
-  var imgPos = $('.me').position();
+// Utility functions
+const getPosition = (selector) => $(selector).position();
+const animateIcon = (icon, startPos, offset, duration) => {
+    $(icon).animate({
+        top: startPos.top + offset.top,
+        left: startPos.left + offset.left
+    }, duration);
+};
 
-  $('i').css({
-    position: 'absolute',
-    zIndex: '1',
-    top: imgPos.top + 100,
-    left: '47%'
-  });
+const returnIcon = (icon, startPos, duration) => {
+    $(icon).animate({
+        top: startPos.top,
+        left: startPos.left
+    }, duration);
+};
 
-  setTimeout(function() {
-    $('#linkedin').animate({
-      top: linkedinPos.top + 10,
-      left: linkedinPos.left - 10
-    }, 500);
-  }, 250);
+// Initialize icons
+const initializeIcons = () => {
+    $('i').hide();
+    
+    $(window).on('load', () => {
+        const positions = {
+            github: getPosition('#github'),
+            linkedin: getPosition('#linkedin'),
+            itch: getPosition('#itch'),
+            img: getPosition('.me')
+        };
 
-  setTimeout(function() {
-    $('#linkedin').animate({
-      top: linkedinPos.top,
-      left: linkedinPos.left
-    }, 250);
+        // Position all icons initially
+        $('i').css({
+            position: 'absolute',
+            zIndex: '1',
+            top: positions.img.top + 100,
+            left: '47%'
+        });
 
-    $('#github').animate({
-      top: githubPos.top + 10,
-      left: githubPos.left - 6
-    }, 500);
-  }, 500);
+        // Animate LinkedIn
+        setTimeout(() => {
+            animateIcon('#linkedin', positions.linkedin, ANIMATION_CONFIG.offset.linkedin, ANIMATION_CONFIG.duration.bounce);
+        }, ANIMATION_CONFIG.delay.linkedin);
 
-  setTimeout(function() {
-    $('#github').animate({
-      top: githubPos.top,
-      left: githubPos.left
-    }, 250);
+        // Animate LinkedIn return and GitHub
+        setTimeout(() => {
+            returnIcon('#linkedin', positions.linkedin, ANIMATION_CONFIG.duration.return);
+            animateIcon('#github', positions.github, ANIMATION_CONFIG.offset.github, ANIMATION_CONFIG.duration.bounce);
+        }, ANIMATION_CONFIG.delay.github);
 
-    $('#itch').animate({
-      top: itchPos.top + 10,
-      left: itchPos.left - 3
-    }, 500);
-  }, 750);
+        // Animate GitHub return and Itch
+        setTimeout(() => {
+            returnIcon('#github', positions.github, ANIMATION_CONFIG.duration.return);
+            animateIcon('#itch', positions.itch, ANIMATION_CONFIG.offset.itch, ANIMATION_CONFIG.duration.bounce);
+        }, ANIMATION_CONFIG.delay.itch);
 
-  setTimeout(function() {
-    $('#itch').animate({
-      top: itchPos.top,
-      left: itchPos.left
-    }, 250);
-})
+        // Animate Itch return
+        setTimeout(() => {
+            returnIcon('#itch', positions.itch, ANIMATION_CONFIG.duration.return);
+        }, ANIMATION_CONFIG.delay.itch + ANIMATION_CONFIG.delay.linkedin);
+
+        $('i').show();
+    });
+};
+
+// Initialize when document is ready
+$(document).ready(initializeIcons);
